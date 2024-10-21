5e21913dda97928ca68473fe14a3db2332261573
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2024-10-22 01:33:27 +0530

    block logging for bots

--- db_logging.py
+++ db_logging.py
@@ -3,9 +3,25 @@ from models import UserClick
 from flask import g
 from config import Config
 
+def is_bot():
+    user_agent = request.headers.get('User-Agent', '').lower()
+    for bot in Config.SCRAPING_BOTS:
+        if bot in user_agent:
+            return True
+    return False
+
 # TODO: Make this asynchronous. We don't care if we lose some of the clicks. 
 def log_product_click(product_id, referrer=None):
-    user_click = UserClick(user_id=g.user_id, session_id=g.session_id, referrer=referrer, product_index=product_id)
+    if is_bot():
+        return
+    user_click = UserClick(
+        user_id=g.user_id, 
+        session_id=g.session_id, 
+        referrer=referrer, 
+        product_index=product_id,
+        ip=request.remote_addr,
+        user_agent=request.headers.get('User-Agent', '')
+    )
 
     if Config.LOGGING_DESTINATION == 'DB':
         try:
@@ -20,7 +36,17 @@ def log_product_click(product_id, referrer=None):
         
         
 def log_search(search_query, referrer=None):
-    user_click = UserClick(user_id=g.user_id, session_id=g.session_id, referrer=referrer, search_query=search_query)
+    if is_bot():
+        return
+
+    user_click = UserClick(
+        user_id=g.user_id, 
+        session_id=g.session_id, 
+        referrer=referrer, 
+        search_query=search_query,
+        ip=request.remote_addr,
+        user_agent=request.headers.get('User-Agent', '')
+    )
     if Config.LOGGING_DESTINATION == 'DB':
         try:
             db.session.add(user_click)
