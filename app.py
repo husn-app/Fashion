3d7090b391515ffa05e71fad6122cdacc094e2ee
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2024-10-23 14:38:42 +0530

    update robots.txt & add privacy/support for ios review

--- app.py
+++ app.py
@@ -55,11 +55,22 @@ def filter_shuffle(seq):
 def inject_deployment_type():
     return dict(deployment_type=app.config['DEPLOYMENT_TYPE'])
 
+# ============================= #
+# Static Routes                 #
+# ============================= #
 @app.route('/robots.txt')
 def robots_txt():
     if Config.DEPLOYMENT_TYPE == 'PROD':
         return app.send_static_file('robots.txt')
     return app.send_static_file('robots-dev.txt')
+
+@app.route('/privacypolicy')
+def privacypolicy():
+    return render_template('privacypolicy.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
     
 
 
